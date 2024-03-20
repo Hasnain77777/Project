0ef864b069782b38fa71d2a0a2b4bca03568d75e
Hasnain77777 <hasnain.h1215@gmail.com.com>
2024-03-21 00:44:32 +0500

    update code

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-let innerContainerDiv = document.querySelector('.inner-container');
+let collectionsDiv = document.querySelector('#collections');
 let preProduct=[];
 let finalProduct=[]
-let collectionDiv; 
 
 let getProduct = async () => {
-    innerContainerDiv.innerHTML=""
     let product = await fetch('https://fakestoreapi.com/products');
     finalProduct = await product.json();
-    collectionDiv=document.querySelector('.inner-container .collections')
     getProdutFromAbove()
 }
 getProduct()
 
 if(window.location.href == "http://127.0.0.1:5500/collections.html"){
+  document.querySelector('.inner-container h1').innerHTML="collections"
   function getProdutFromAbove(){
     finalProduct.forEach(element => {
-      console.log('fd');
       if(!preProduct.includes(element.category)){
         preProduct.push(element.category)
-        innerContainerDiv.innerHTML +=`
-        <div data-category=${element.category}  onclick="storeD(this)">
+        collectionsDiv.innerHTML +=`<a href="collections.html?cat=${element.category}" data-category="${element.category}" onclick="storeD(this)">
         <img src="${element.image}" alt="">
         <h3>${element.category}</h3>
-        </div>`
+        </a>`
+        console.log(element.category);
       }
     });
   }
   function storeD(get){
-    storeCategory.push(get.getAttribute('data-category'))
-    window.location.search=`?cat=${get.getAttribute('data-category')}`
+    localStorage.setItem('Category',get.getAttribute('data-category'));
   }
 }
-else if(window.location.href == `http://127.0.0.1:5500/collections.html${storeCategory[0]}`){
-  storeCategory=[]
+else if(decodeURI(window.location.search) == `?cat=${localStorage.getItem('Category')}`){
   function getProdutFromAbove(){
     const queryString = window.location.search;
     const decoded = decodeURI(queryString);
+    console.log(decoded);
     const newDecode = decoded.replace('?cat=','')
     finalProduct.forEach(element => {
     if(element.category == newDecode){
       const decoded = decodeURI(queryString);
       preProduct.push(element.category)
-      innerContainerDiv.innerHTML +=`<a href="products.html?id=${element.id}"onclick="storeD(this)" class="collections" data-category=${element.category}>
+      collectionsDiv.innerHTML +=`<a href="products.html?id=${element.id}" class="collections" onclick="storeD(this)" data-category=${element.category}>
       <img src="${element.image}" alt="">
-      <h3>${element.category}</h3>
-      <h3>${element.price}$</h3>
+      <div class='productContent'>
+        <h3>${element.title}</h3>
+        <div class="priceRate">
+          <h5>Price: <span>${element.price}$</span></h5>
+          <h5>Rating:${element.rating.rate}</h5>
+        </div>
+      </div>
       </a>` 
     }
     
@@ -52,17 +53,18 @@ else if(window.location.href == `http://127.0.0.1:5500/collections.html${storeCa
   } 
 }
 else{
-  innerContainerDiv.innerHTML =`<div id="back">
-  <h1>PAGE NOT FOUND</h1>
+  console.log(localStorage.getItem('Category'));
+  document.querySelector('.collectionsContainer').innerHTML=''
+  document.querySelector('.pageNotFound').innerHTML =`
+  <h1>page not found</h1>
   <div class="backButton">
   <a href="collections.html">Back to Collections</a>
-  </div>
   </div>` 
 }
 
 
 
-
+localStorage.setItem("lastname", "Sm ith");
 
 
 
